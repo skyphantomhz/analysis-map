755c011d3050e0769d9416e1b8ecbcb3c826c6d4
dac tri <skyphantomhz@gmail.com>
2018-03-25 08:33:35 +0700

    Config deploy heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyDTxh74YroXZzv65e5QKSilyxV_5mHzo6Qc",
+    authDomain: "eventomorrow-dev-deploy.firebaseio.com",
+    databaseURL: "https://eventomorrow-dev-deploy.firebaseio.com/",
+    projectId: "eventomorrow-dev-deploy",
+    storageBucket: "eventomorrow-dev-deploy.appspot.com",
+    messagingSenderId: "613915763145"
+  },
+  
+  googleMapsKey: "AIzaSyCoH8CWRcdupbhcdECOTLvFlF-SeJ3qM54"
 };
